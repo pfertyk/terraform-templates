b9cc08852a61297ed9c74c6e8bf2421360bd0c87
Paweł Fertyk <pfertyk@pfertyk.me>
2023-02-15 23:27:39 +0100

    Add AWS Lambda template

---
+++ lambda/config.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_version = "= 1.2.7"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
---
+++ lambda/main.tf
@@ -0,0 +1,64 @@
+data "archive_file" "code" {
+  type        = "zip"
+  source_dir  = "${path.module}/code"
+  output_path = "${path.module}/code.zip"
+}
+
+data "archive_file" "layer" {
+  type        = "zip"
+  source_dir  = "${path.module}/layer"
+  output_path = "${path.module}/layer.zip"
+  depends_on  = [null_resource.pip_install]
+}
+
+resource "aws_iam_role" "iam_role" {
+  name = "lambda_iam_role"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_lambda_layer_version" "layer" {
+  layer_name          = "test-layer"
+  filename            = data.archive_file.layer.output_path
+  source_code_hash    = data.archive_file.layer.output_base64sha256
+  compatible_runtimes = ["python3.9", "python3.8", "python3.7", "python3.6"]
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name    = "test-lambda"
+  handler          = "lambda.main"
+  runtime          = "python3.9"
+  filename         = data.archive_file.code.output_path
+  source_code_hash = data.archive_file.code.output_base64sha256
+  role             = aws_iam_role.iam_role.arn
+  layers           = [aws_lambda_layer_version.layer.arn]
+  environment {
+    variables = {
+      "MESSAGE" = "Terraform sends its regards"
+    }
+  }
+}
+
+resource "null_resource" "pip_install" {
+  triggers = {
+    shell_hash = "${sha256(file("${path.module}/requirements.txt"))}"
+  }
+
+  provisioner "local-exec" {
+    command = "python3 -m pip install -r requirements.txt -t ${path.module}/layer"
+  }
+}
