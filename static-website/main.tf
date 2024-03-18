30084c5ddfbc53c44be5793715fd947b408c97b5
Paweł Fertyk <pfertyk@pfertyk.me>
2024-03-18 23:25:39 +0100

    Fix S3 policy issue

--- static-website/main.tf
+++ static-website/main.tf
@@ -2,8 +2,18 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "pfertyk-test-website"
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
+resource "aws_s3_bucket_public_access_block" "bucket_access_block" {
   bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  depends_on = [aws_s3_bucket_public_access_block.bucket_access_block]
+  bucket     = aws_s3_bucket.bucket.id
   policy = jsonencode(
     {
       "Version" : "2012-10-17",
@@ -26,7 +36,7 @@ resource "aws_s3_object" "file" {
   key          = replace(each.value, "/^content//", "")
   source       = each.value
   content_type = lookup(local.content_types, regex("\\.[^.]+$", each.value), null)
-  etag         = filemd5(each.value)
+  source_hash  = filemd5(each.value)
 }
 
 resource "aws_s3_bucket_website_configuration" "hosting" {
